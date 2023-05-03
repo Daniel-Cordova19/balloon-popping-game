5b2f0479c29fb82e2137d60760baa0c948393f48
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-03 14:21:14 +0200

    avanzado

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // tu código aquí
+
+// Usad adecadamente querySelectorAll y el método forEach para asociar el evento 'mouseover' , y que me mustre por console.log el 'globo' actual sobre el cual estamos planeando. Pista: necesitais también usar el objeto event
+document.querySelectorAll(".balloon").forEach((b) =>
+  b.addEventListener("mouseover", (event) => {
+    const balloon = event.target;
+    balloon.style.backgroundColor = "#ededed";
+    balloon.innerHTML = "POP!";
+  })
+);
